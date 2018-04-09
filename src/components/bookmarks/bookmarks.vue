7f6e98ed133a5321d64b8854c659fd6db2fd6fdb
WangShuai <hansmking@163.com>
2018-04-09 23:43:19 +0800

    更新右侧列表样式

--- src/components/bookmarks/bookmarks.vue
+++ src/components/bookmarks/bookmarks.vue
@@ -3,7 +3,7 @@
     <div class="bookmarks-wrapper">
       <ul class="left-wrapper">
         <li class="top-item" v-for="(bookmark, index) in bookmarks" :key="index">
-          <a @click="tabNav" href="javascript:;" class="top-name"><i class="icon" :class="bookmark.icon"></i>{{bookmark.name}}</a>
+          <a href="javascript:;" class="top-name"><i class="icon" :class="bookmark.icon"></i>{{bookmark.name}}</a>
           <ul class="sub-list" v-if="bookmark.sublist || bookmark.sublist.length>0">
             <li class="sub-item" v-for="(sub, index) in bookmark.sublist" :key="index">
               <a href="javascript:;" class="sub-name">{{sub.name}}</a>
@@ -12,16 +12,16 @@
         </li>
       </ul>
       <div class="content-wrapper">
-        <div class="content-item" v-for="(item, index) in curList" :key="index">
+        <a class="content-item" v-for="(item, index) in curList" :key="index" :href="item.url" target="_blank">
           <div class="top-wrapper">
-            <span class="pic"><img :src="item.pic"></span>
+            <span class="pic"><img width="40" height="40" :src="item.pic"></span>
             <div class="title">
               <span class="name">{{item.name}}</span>
               <span class="url">{{item.url}}</span>
             </div>
           </div>
           <div class="description">{{item.description}}</div>
-        </div>
+        </a>
       </div>
     </div>
   </div>
@@ -33,9 +33,9 @@
     data() {
       return {
         bookmarks: [],
-        curList: [],
-        curTopIndex: 0,
-        curSubIndex: 0
+        // curList: [],
+        curTopIndex: -1,
+        curSubIndex: -1
       }
     },
     created() {
@@ -46,15 +46,29 @@
         }
       })
     },
-    methods: {
-      tabNav() {
-        this.curList = this.bookmarks[this.curTopIndex].sublist[this.curSubIndex].items
+    computed: {
+      curList() {
+        var curArr = []
+        if (this.curTopIndex === -1) {
+          for (var topli of this.bookmarks) {
+            for (var subli of topli.sublist) {
+              for (var item of subli.items) {
+                if (curArr.includes(item)) {
+                  curArr.push(item)
+                }
+              }
+            }
+          }
+        }
+        return curArr
       }
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+body
+  background: #eee
   .bookmarks
     margin-top: 48px
     padding-top: 50px
@@ -98,4 +112,61 @@
       .content-wrapper
         display: flex
         flex: 1
+        flex-wrap: wrap
+        .content-item
+          flex: 0 0 23%;
+          margin: 6px 1%
+          display: inline-block
+          box-sizing: border-box
+          padding: 13px 15px 15px 15px
+          overflow: hidden
+          background: #f6f6f6
+          border-radius: 6px
+          transition: all 0.2s
+          box-shadow: 0px 4.33px 5px 0px rgba( 111, 111, 111, 0.1 )
+          &:hover
+            box-shadow: 0px 4.33px 5px 0px rgba( 1, 1, 1, 0.25 )
+          .top-wrapper
+            position: relative
+            height: 40px
+            .pic
+              position: absolute
+              display: block
+              left: 0
+              top: 0
+              width: 40px
+              height: 40px
+              border-radius: 50%
+            .title
+              display: block
+              padding-left: 50px
+              font-size: 0
+              .name
+                display: block
+                width:100%
+                height: 14px
+                line-height: 14px
+                padding-top: 9px
+                overflow: hidden
+                text-overflow: ellipsis
+                white-space: nowrap
+                font-size: 14px
+                font-weight: 700
+                color: #46474f
+              .url
+                display: block
+                width:100%
+                height: 14px
+                line-height: 14px
+                overflow: hidden
+                text-overflow: ellipsis
+                white-space: nowrap
+                font-size: 12px
+                color: #a4a4a4
+          .description
+            height: 32px
+            line-height: 16px
+            padding: 10px 0 0 5px
+            font-size: 12px
+            color: #8c8d8f
 </style>
